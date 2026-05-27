2029aaf2fafd5d173e990051f16241635303d0cb
Rashmikanta <rashmikanta131@gmail.com>
2026-05-27 19:54:34 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -22,18 +22,17 @@ wget https://github.com/kubernetes/kops/releases/download/v1.32.0/kops-linux-amd
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
-aws s3api create-bucket --bucket reyaz-kops-testbkt143333.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt143333.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt143333.k8s.local
-kops create cluster --name=reyaz.k8s.local --zones=ap-south-1a,ap-south-1b --control-plane-count=1 --control-plane-size=t3.medium --node-count=2 --node-size=t3.small --node-volume-size=20 --control-plane-volume-size=20 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico --topology=public
-kops update cluster --name reyaz.k8s.local --yes --admin
-
+aws s3api create-bucket --bucket babul-prvt-bkt58 --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket babul-prvt-bkt58 --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://babul-prvt-bkt58
+kops create cluster --name=babul.k8s.local --zones=ap-south-1a,ap-south-1b --control-plane-count=1 --control-plane-size=t3.medium --node-count=2 --node-size=t3.micro --node-volume-size=20 --control-plane-volume-size=20 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico --topology=public
+kops update cluster --name babul.k8s.local --yes --admin
 
 wq!
 
 sh kops.sh
 
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt143333.k8s.local
+export KOPS_STATE_STORE=s3://babul-prvt-bkt58
 
 kops validate cluster --wait 10m
 
